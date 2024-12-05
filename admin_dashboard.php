4da022a04e5c2ae9758529ec51ee46d3d1530ded
juanWijaya0603 <juanw0603@gmail.com>
2024-12-05 16:34:02 +0700

    update 2

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/admin_dashboard.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>ADMIN DASHBOARD</h1>
+<section class="layout">
+  <div class="sidebar-menu">1</div>
+  <div class="main-menu">2</div>
+</section>
 </body>
 </html>
